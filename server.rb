3f0bd73223f1fc2847e655a8be367b1bdd98d92d
Kevin Thompson <kevin.david.thompson@gmail.com>
2015-08-12 10:20:02 -0500

    refactors and cleans up code a bit

--- server.rb
+++ server.rb
@@ -5,6 +5,22 @@ ENROLL_RESPONSE = {
     "node_key": "this_is_a_node_secret"
 }
 
+def config_getter(filename="default")
+  if ENV["RACK_ENV"] == "test"
+    config_folder = "test_files"
+  else
+    config_folder = "osquery_configs"
+  end
+
+  file_to_get = File.join(config_folder, "#{filename}.conf")
+
+  if File.exist?(file_to_get)
+    File.read(file_to_get)
+  else
+    File.read(File.join(config_folder, "default.conf"))
+  end
+end
+
 get '/status' do
   "running at #{Time.now}"
 end
@@ -18,26 +34,11 @@ post '/api/enroll' do
 end
 
 post '/api/config' do
-  if ENV["RACK_ENV"] == "test"
-    config_folder = "test_files"
-  else
-    config_folder = "osquery_configs"
-  end
-  File.read(File.join(config_folder, "default.conf"))
+  config_getter
 end
 
 post '/api/config/:name' do
-  if ENV["RACK_ENV"] == "test"
-    config_folder = "test_files"
-  else
-    config_folder = "osquery_configs"
-  end
-  file_to_get = File.join(config_folder, "#{params['name']}.conf")
-  if File.exist?(file_to_get)
-    File.read(file_to_get)
-  else
-    File.read(File.join(config_folder, "default.conf"))
-  end
+  config_getter(params['name'])
 end
 
 post '/' do
