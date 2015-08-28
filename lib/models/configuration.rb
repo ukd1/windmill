142c197f3fdb0c21f4295335797b75e5418460c2
Kevin Thompson <kevin.david.thompson@gmail.com>
2015-08-28 13:41:11 -0500

    adds validations to the configuration model

--- lib/models/configuration.rb
+++ lib/models/configuration.rb
@@ -1,3 +1,4 @@
+require 'json'
 require 'sinatra/activerecord'
 require_relative '../../environments'
 
@@ -6,4 +7,16 @@ class Configuration < ActiveRecord::Base
   # version, integer
   # config_json, text
   # notes, text
+
+  validates :name, :version, :config_json, presence: true
+  validate :json_validator
+
+  def json_validator
+    begin
+      JSON.parse!(self.config_json)
+    rescue
+      errors.add(:config_json, "Not parsable JSON")
+    end
+    true
+  end
 end
--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -5,7 +5,7 @@ require_relative '../server.rb'
 
 describe "osquery configuration files" do
   before do
-    @config = Configuration.new
+    @config = Configuration.new(name:"test", version:1, notes:"test", config_json: {test:"test"}.to_json)
   end
 
   subject {@config}
@@ -14,4 +14,29 @@ describe "osquery configuration files" do
   it { should respond_to :version }
   it { should respond_to :config_json }
   it { should respond_to :notes }
+
+  it "should require a name" do
+    expect(@config.valid?).to be_truthy
+    @config.name = nil
+    expect(@config.valid?).to be_falsey
+  end
+
+  it "should require a version" do
+    expect(@config.valid?).to be_truthy
+    @config.version = nil
+    expect(@config.valid?).to be_falsey
+  end
+
+  it "should require a config_json" do
+    expect(@config.valid?).to be_truthy
+    @config.config_json = nil
+    expect(@config.valid?).to be_falsey
+  end
+
+  it "should require valid json in config_json" do
+    expect(@config.valid?).to be_truthy
+    @config.config_json = "not valid json"
+    expect(@config.valid?).to be_falsey
+  end
+
 end
